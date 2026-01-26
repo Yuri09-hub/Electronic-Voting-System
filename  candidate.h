eef5ef400db35c740709d35112cec1624065cb51
Yuri Rodrigues <yurihenriques2004@gmail.com>
2026-01-26 15:02:18 +0100

    update

---  candidate.h
+++  candidate.h
@@ -4,5 +4,13 @@
 
 #ifndef CANDIDATE_H
 #define CANDIDATE_H
+typedef struct no {
+    char name[50];
+    int id;
+    int number_of_votes;
+    struct node *next;
+}candidate;
+
+candidate *create_candidate(char *name, int id, int number_of_votes);
 
 #endif //CANDIDATE_H
