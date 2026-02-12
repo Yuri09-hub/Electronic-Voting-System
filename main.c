332577e2b7d7728f4d7e3666b09b9183babd04b9
Yuri Rodrigues <yurihenriques2004@gmail.com>
2026-02-12 10:13:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,25 +11,27 @@
 
     void display_menu(){
         printf("\n");
-        printf("  ╔══════════════════════════════════════════╗\n");
-        printf("  ║          VOTING SYSTEM MANAGER           ║\n");
-        printf("  ╠══════════════════════════════════════════╣\n");
-        printf("  ║  1.Register voter                        ║\n");
-        printf("  ║  2.update voter                          ║\n");
-        printf("  ║  3.Register candidate                    ║\n");
-        printf("  ║  4.Remove candidate                      ║\n");
-        printf("  ║  5. Vote                                 ║\n");
-        printf("  ║  6. voter Leave queue (give up)          ║\n");
-        printf("  ║  7. Show Queue Size                      ║\n");
-        printf("  ║  5. Vote                                 ║\n");
-        printf("  ║  0. Exit                                 ║\n");
-        printf("  ╚══════════════════════════════════════════╝\n");
+        printf("  ╔═══════════════════════════════════════════════╗\n");
+        printf("  ║          VOTING SYSTEM MANAGER                ║\n");
+        printf("  ╠═══════════════════════════════════════════════╣\n");
+        printf("  ║  1.Register voter                             ║\n");
+        printf("  ║  2.update voter                               ║\n");
+        printf("  ║  3.Register candidate                         ║\n");
+        printf("  ║  4.Remove candidate                           ║\n");
+        printf("  ║  5.Initialize vote                            ║\n");
+        printf("  ║  6.Voter Leave queue (give up)                ║\n");
+        printf("  ║  7.Show Queue Size                            ║\n");
+        printf("  ║  9.Pri                                        ║\n");
+        printf("  ║  8.Vote                                       ║\n");
+        printf("  ║  8.Vote                                       ║\n");
+        printf("  ║  0. Exit                                      ║\n");
+        printf("  ╚═══════════════════════════════════════════════╝\n");
         printf("  Selection > ");
 }
 
 int main(){
 
-        
+
 
 
 
